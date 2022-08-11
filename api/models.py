d531ae58d248a53e1f9bbdad8c1eb64cd6fa8209
Ben Banting <bbanting@tuta.io>
2022-08-11 12:18:32 -0600

    Some more small model tweaks

--- api/models.py
+++ api/models.py
@@ -20,6 +20,12 @@ class Root(models.Model):
 
     class Meta:
         ordering = ["text"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["text", "helpset"], 
+                name="root_text_unique",
+            )
+        ]
 
     def __str__(self) -> str:
         return f"{self.text} ({self.helpset})"
@@ -31,14 +37,20 @@ class Lexeme(models.Model):
     # that still has linked words. Words that have no other links
     # should be deleted.
     helpset = models.ForeignKey(HelpSet, models.CASCADE, related_name="lexemes")
-    root = models.ForeignKey(Root, models.SET_NULL, null=True, blank=True)
     text = models.CharField(max_length=100)
+    root = models.ForeignKey(Root, models.SET_NULL, null=True, blank=True)
     help_text = models.TextField(null=True, blank=True)
     help_image = models.ImageField(default="", blank=True)
     words = models.ManyToManyField("Word", related_name="lexemes", through="Link")
 
     class Meta:
         ordering = ["text"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["text", "helpset"], 
+                name="lexeme_text_unqiue",
+            )
+        ]
 
     def __str__(self) -> str:
         return f"{self.text} ({self.helpset})"
@@ -46,21 +58,17 @@ class Lexeme(models.Model):
 
 class Word(models.Model):
     """A concrete manifestation of a lexeme."""
+    helpset = models.ForeignKey(HelpSet, models.CASCADE, related_name="words")
     text = models.CharField(max_length=100)
-
-    @property
-    def helpset(self) -> HelpSet | None:
-        """Retrieve the helpset it belongs to."""
-        try:
-            lex = self.lexemes.get()
-        except self.DoesNotExist:
-            pass
-        else:
-            return lex.helpset
-        return None
 
     class Meta:
         ordering = ["text"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["helpset", "text"], 
+                name="word_text_unique",
+            )
+        ]
 
     def __str__(self) -> str:
         return f"{self.text} ({self.helpset})"
@@ -74,5 +82,10 @@ class Link(models.Model):
     word = models.ForeignKey(Word, models.CASCADE)
     parse_data = models.CharField(max_length=200, blank=True, default="")  
 
+    @property
+    def helpset(self) -> HelpSet | None:
+        """Retrieve the helpset it belongs to."""
+        return self.lexeme.helpset
+
     def __str__(self) -> str:
-        return f"{self.word.text} -> {self.lexeme.text} ({self.lexeme.helpset})"
+        return f"{self.word.text} -> {self.lexeme.text} ({self.helpset})"
