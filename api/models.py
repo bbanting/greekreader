08c9aa4cf735a2d7ba288e0c6f2721707157e0e2
Ben Banting <bbanting@tuta.io>
2022-08-11 10:22:44 -0600

    Made __str__ methods for models

--- api/models.py
+++ api/models.py
@@ -5,15 +5,21 @@ from django.contrib.auth.models import User
 class HelpSet(models.Model):
     """A set of lexical helps."""
     name = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, models.SET_NULL, null=True)
     date_created = models.DateField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True) # NOTE: Set when altering Lexeme
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Root(models.Model):
     """A lexical root. Used for grouping together related lexemes."""
-    helpset = models.ForeignKey(HelpSet, on_delete=models.CASCADE)
+    helpset = models.ForeignKey(HelpSet, models.CASCADE)
     text = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.text} ({self.helpset})"
 
 
 class Lexeme(models.Model):
@@ -21,12 +27,15 @@ class Lexeme(models.Model):
     # NOTE: User should be warned when deleting a lexeme 
     # that still has linked words. Words that have no other links
     # should be deleted.
-    helpset = models.ForeignKey(HelpSet, on_delete=models.CASCADE)
-    root = models.ForeignKey(Root, on_delete=models.SET_NULL, null=True, blank=True)
+    helpset = models.ForeignKey(HelpSet, models.CASCADE, related_name="lexemes")
+    root = models.ForeignKey(Root, models.SET_NULL, null=True, blank=True)
     text = models.CharField(max_length=100)
     help_text = models.TextField(null=True, blank=True)
     help_image = models.ImageField()
     words = models.ManyToManyField("Word", related_name="lexemes", through="Link")
+
+    def __str__(self) -> str:
+        return f"{self.text} ({self.helpset})"
 
 
 class Word(models.Model):
@@ -44,11 +53,17 @@ class Word(models.Model):
             return lex.helpset
         return None
 
+    def __str__(self) -> str:
+        return f"{self.text} ({self.helpset})"
+
 
 class Link(models.Model):
     """A link between a word and the lexeme it is derived from.
     Distinct lexemes may have forms that are morphologically identical.
     """
-    lexeme = models.ForeignKey(Lexeme, on_delete=models.CASCADE)
-    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    lexeme = models.ForeignKey(Lexeme, models.CASCADE)
+    word = models.ForeignKey(Word, models.CASCADE)
     parse_data = models.CharField(max_length=200, blank=True, default="")
+
+    def __str__(self) -> str:
+        return f"{self.word.text} -> {self.lexeme.text} ({self.lexeme.helpset})"
