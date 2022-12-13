e200cca3120f99a07e689178dec4ae81ba8c3143
Ben Banting <bbanting@tuta.io>
2022-12-13 12:55:09 -0600

    Created admin_view_factory() t, removed tons of repeated code in that file

--- api/views/adminonly.py
+++ api/views/adminonly.py
@@ -1,116 +1,35 @@
-from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .. import models, serializers
 
 
-class HelpSetList(generics.ListCreateAPIView):
-    """List current helpsets or create a new helpset."""
-    queryset = models.HelpSet.objects.all()
-    serializer_class = serializers.HelpSetSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
+def admin_view_factory(model, serializer) -> tuple[ListCreateAPIView, RetrieveUpdateDestroyAPIView]:
+    attrs = {
+        "queryset": model.objects.all(),
+        "serializer_class": serializer,
+        "permission_classes": [permissions.IsAdminUser],
+        }
+
+    return (
+        type(
+            f"{model.__name__}List", 
+            (ListCreateAPIView,), 
+            {**attrs, "__doc__": f"List current {model}s or create a new one."}
+        ),
+        type(
+            f"{model.__name__}Detail", 
+            (RetrieveUpdateDestroyAPIView,), 
+            {**attrs, "__doc__": f"View or modify a {model}."}
+        ),
+    )
 
 
-class HelpSetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modify one particular helpset."""
-    queryset = models.HelpSet.objects.all()
-    serializer_class = serializers.HelpSetSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class RootList(generics.ListCreateAPIView):
-    """List roots or create a new root."""
-    queryset = models.Root.objects.all()
-    serializer_class = serializers.RootSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class RootDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular root."""
-    queryset = models.Root.objects.all()
-    serializer_class = serializers.RootSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class LexemeList(generics.ListCreateAPIView):
-    """List lexemes or create a lexeme."""
-    queryset = models.Lexeme.objects.all()
-    serializer_class = serializers.LexemeSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class LexemeDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular lexeme."""
-    queryset = models.Lexeme.objects.all()
-    serializer_class = serializers.LexemeSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-    
-
-class HelpImageList(generics.ListCreateAPIView):
-    """List help images or create one."""
-    queryset = models.HelpImage.objects.all()
-    serializer_class = serializers.HelpImageSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-    
-
-class HelpImageDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modify a help image."""
-    queryset = models.HelpImage.objects.all()
-    serializer_class = serializers.HelpImageSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class WordList(generics.ListCreateAPIView):
-    """List words or create a new word."""
-    queryset = models.Word.objects.all()
-    serializer_class = serializers.WordSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class WordDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular word."""
-    queryset = models.Word.objects.all()
-    serializer_class = serializers.WordSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class BookList(generics.ListCreateAPIView):
-    """List books or create a new book."""
-    queryset = models.Book.objects.all()
-    serializer_class = serializers.BookSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular book."""
-    queryset = models.Book.objects.all()
-    serializer_class = serializers.BookSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class ChapterList(generics.ListCreateAPIView):
-    """List chapters or create a new chapter."""
-    queryset = models.Chapter.objects.all()
-    serializer_class = serializers.ChapterSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class ChapterDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular chapter."""
-    queryset = models.Chapter.objects.all()
-    serializer_class = serializers.ChapterSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class StudyGroupList(generics.ListCreateAPIView):
-    """List study groups or create a new one."""
-    queryset = models.StudyGroup.objects.all()
-    serializer_class = serializers.StudyGroupSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
-
-
-class StudyGroupDetail(generics.RetrieveUpdateDestroyAPIView):
-    """View or modifiy one particular study group."""
-    queryset = models.StudyGroup.objects.all()
-    serializer_class = serializers.StudyGroupSerializerAdmin
-    permission_classes = [permissions.IsAdminUser]
+HelpSetList, HelpSetDetail = admin_view_factory(models.HelpSet, serializers.HelpSetSerializerAdmin)
+RootList, RootDetail = admin_view_factory(models.Root, serializers.RootSerializerAdmin)
+LexemeList, LexemeDetail = admin_view_factory(models.Lexeme, serializers.LexemeSerializerAdmin)
+HelpImageList, HelpImageDetail = admin_view_factory(models.HelpImage, serializers.HelpImageSerializerAdmin)
+WordList, WordDetail = admin_view_factory(models.Word, serializers.WordSerializerAdmin)
+BookList, BookDetail = admin_view_factory(models.Book, serializers.BookSerializerAdmin)
+ChapterList, ChapterDetail = admin_view_factory(models.Chapter, serializers.ChapterSerializerAdmin)
+StudyGroupList, StudyGroupDetail = admin_view_factory(models.StudyGroup, serializers.StudyGroupSerializerAdmin)
