921c5f816ec865430578ab3ce3a81bfc183d9566
Benjamin Banting <bbanting@tuta.io>
2022-12-19 16:45:42 -0600

    Created basic app shell for editor

--- frontend/editor/src/App.tsx
+++ frontend/editor/src/App.tsx
@@ -1,11 +1,17 @@
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, AppShell, Header, Navbar, Text } from '@mantine/core';
 
 
 function App() {
   return (
     <MantineProvider withNormalizeCSS withGlobalStyles>
       <div className="App">
-        Editor app goes here.
+        <AppShell 
+          padding="md" 
+          navbar={<Navbar width={{base: 300}} p="xs">Asset selection goes here</Navbar>}
+          header={<Header height={50} p="xs">Header placeholder</Header>}
+        >
+          <Text sx={{textAlign: "center"}}>Toolbar goes here.</Text>
+        </AppShell>
       </div>
     </MantineProvider>
   );
