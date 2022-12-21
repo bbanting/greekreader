e967700026527438c1e56bb2e8e20340bb0904bc
Benjamin Banting <bbanting@tuta.io>
2022-12-21 13:23:03 -0600

    Created the HelpSet interface, fetched the data from api for AssetSelector

--- frontend/editor/src/components/AssetSelector.tsx
+++ frontend/editor/src/components/AssetSelector.tsx
@@ -15,15 +15,30 @@ interface Book {
   fallback_helpset: number | null
 }
 
+interface HelpSet {
+  id: number,
+  settings: number | null,
+  date_created: string,
+  last_modified: string,
+  name: string,
+  creator: number
+}
+
 
 export function AssetSelector() {
   /**A component to select which asset to edit. */
     const [books, setBooks] = useState<Book[] | null>(null);
+    const [helpSets, setHelpSets] = useState<HelpSet[] | null>(null);
 
     useEffect(() => {
       fetch("http://localhost:8000/api/edit/books/")
       .then(res => {console.log(res); return res.json();})
       .then(data => setBooks(data))
+      .catch((error) => console.log(error));
+
+      fetch("http://localhost:8000/api/edit/helpsets/")
+      .then(res => {console.log(res); return res.json();})
+      .then(data => setHelpSets(data))
       .catch((error) => console.log(error));
     }, [])
 
@@ -45,7 +60,7 @@ export function AssetSelector() {
           </Accordion.Control>
           <Accordion.Panel>
             <ul>
-              <li>Placeholder</li>
+              {helpSets?.map(hs => <li>{hs.name}</li>)}
             </ul>
           </Accordion.Panel>
         </Accordion.Item>
