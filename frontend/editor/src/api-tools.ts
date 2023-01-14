b62a1c1baeb796c3793141ce987d9d4d4d06ce51
Ben Banting <bbanting@tuta.io>
2023-01-14 11:37:54 -0600

    Added django-cors-headers to Pipfile; Added BASE_URL in api-tools.ts

--- frontend/editor/src/api-tools.ts
+++ frontend/editor/src/api-tools.ts
@@ -1,32 +1,33 @@
 import { Book, Chapter, HelpSet } from "./api-types";
 
+const BASE_URL = "http://localhost:8000/api/edit/";
 
 /**Get all books from the api. */
 export async function getBooks(): Promise<Book[]> {
-  const res = await fetch("http://localhost:8000/api/edit/books/");
+  const res = await fetch(BASE_URL + "books/");
   return await res.json();
 }
 
 /**Get a single book from the api. */
 export async function getSingleBook(id:number): Promise<Book> {
-  const res = await fetch(`http://localhost:8000/api/edit/books/${id}/`);
+  const res = await fetch(BASE_URL + `books/${id}/`);
   return await res.json();
 }
   
 /**Get all help sets from the api. */
 export async function getHelpsets(): Promise<HelpSet[]> {
-  const res = await fetch("http://localhost:8000/api/edit/helpsets/");
+  const res = await fetch(BASE_URL + "helpsets/");
   return await res.json();
 }
 
 /**Get a single help set from the api. */
 export async function getSingleHelpset(id:number): Promise<HelpSet> {
-  const res = await fetch(`http://localhost:8000/api/edit/helpsets/${id}/`);
+  const res = await fetch(BASE_URL + `helpsets/${id}/`);
   return await res.json();
 }
 
 /**Get a single chapter from the api. */
 export async function getSingleChapter(id: number): Promise<Chapter> {
-  const res = await fetch(`http://localhost:8000/api/edit/chapters/${id}/`);
+  const res = await fetch(BASE_URL + `chapters/${id}/`);
   return await res.json();
 }
